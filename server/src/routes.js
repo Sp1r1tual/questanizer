4e90bb9d06db54cdae4efa0a3aa036b99f8e6839
Sp1r1tual <andriy0383@gmail.com>
2025-07-24 10:00:06 +0300

    fix(backend): Fix routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,19 +6,21 @@ import authRouter from "./auth/router/auth-router.js";
 import tasksRouter from "./tasks/router/tasks-router.js";
 import userStatsRouter from "./stats/router/user-stats-router.js";
 import bossRouter from "./boss/router/boss-router.js";
+import userRouter from "./user/router/user-router.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const routes = (app) => {
-    app.use("/api", authRouter);
-    app.use("/api", tasksRouter);
-    app.use("/api", userStatsRouter);
-    app.use("/api", bossRouter);
     app.use(
         "/api/public",
         express.static(path.resolve(__dirname, "../public"))
     );
+    app.use("/api", authRouter);
+    app.use("/api", userRouter);
+    app.use("/api", tasksRouter);
+    app.use("/api", userStatsRouter);
+    app.use("/api", bossRouter);
 };
 
 export default routes;
