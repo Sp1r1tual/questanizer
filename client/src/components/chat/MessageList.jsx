5eaabf2cb2ffb05dfca646146c71013e1ab30657
Sp1r1tual <andriy0383@gmail.com>
2025-08-15 20:29:03 +0300

    feat(frontend): Add a chat stick to the bottom

--- client/src/components/chat/MessageList.jsx
+++ client/src/components/chat/MessageList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
 import { Message } from "./Message";
@@ -5,26 +6,36 @@ import { Message } from "./Message";
 import styles from "./MessageList.module.css";
 
 const MessageList = ({ messages, currentUserId, onRemoveMessage }) => {
+    const bottomRef = useRef(null);
+
     const { t } = useTranslation();
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
 
     return (
         <div className={styles.messageListWrapper}>
             {messages.length === 0 ? (
                 <p>{t("chat.noMessages")}</p>
             ) : (
-                messages.map((msg) => {
-                    const isMine = String(msg.from) === String(currentUserId);
+                <>
+                    {messages.map((msg) => {
+                        const isMine =
+                            String(msg.from) === String(currentUserId);
 
-                    return (
-                        <Message
-                            key={msg.id}
-                            message={msg}
-                            isMine={isMine}
-                            currentUserId={currentUserId}
-                            onRemove={() => onRemoveMessage(msg.id)}
-                        />
-                    );
-                })
+                        return (
+                            <Message
+                                key={msg.id}
+                                message={msg}
+                                isMine={isMine}
+                                currentUserId={currentUserId}
+                                onRemove={() => onRemoveMessage(msg.id)}
+                            />
+                        );
+                    })}
+                    <div ref={bottomRef} />
+                </>
             )}
         </div>
     );
