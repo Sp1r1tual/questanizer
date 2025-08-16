0c3ff2cc7803d1ba59db5ee3a4425ab4d334e413
Sp1r1tual <andriy0383@gmail.com>
2025-08-16 12:16:57 +0300

    feat(backend): Add four new bosses and optimize game balance

--- server/src/shared/configs/user-stats-config.js
+++ server/src/shared/configs/user-stats-config.js
@@ -1,8 +1,8 @@
 const DIFFICULTY_REWARDS = {
-    easy: { xp: 10, damage: 5, gold: 2 },
-    medium: { xp: 20, damage: 10, gold: 4 },
-    hard: { xp: 40, damage: 20, gold: 8 },
-    critical: { xp: 80, damage: 40, gold: 16 },
+    easy: { xp: 5, damage: 5, gold: 1 },
+    medium: { xp: 7, damage: 10, gold: 2 },
+    hard: { xp: 15, damage: 20, gold: 3 },
+    critical: { xp: 25, damage: 35, gold: 4 },
 };
 
 export { DIFFICULTY_REWARDS };
