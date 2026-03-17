67daf8729726340bb21fd06009181bb23ceade3b
Sp1r1tual <andriy0383@gmail.com>
2026-03-17 17:59:59 +0200

    chore(fullstack): configure docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,8 +19,11 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
     strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    host: true,
   },
   build: {
     target: "esnext",
